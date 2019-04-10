a36476d4ff940eace9cfe19adb2434d1dc5d4b89
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-10 00:58:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,6 +22,12 @@ class Song
     song
   end
 
+  def self.create_by_name(name)
+  song=self.create
+  song.name=name
+  song
+end
+
   def self.find_by_name(name)
     self.all.find{|song|song.name == name}
   end
